10f934d0a548969b7bf9f4ded75b59053138bac9
RayWu <raywu1988@me.com>
2022-05-29 22:39:27 +0100

    init practice of s19_Todoey

---
+++ s19_Todoey-iOS13/Todoey/TodoListController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Egg", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    //MARK - Tableview Datasource Methods
+    
+
+
+}
+
